a2e24647a7c3043dd826fc767f430357ea3604c3
Lee Richardson <leearthurrichardson@hotmail.com>
2018-04-29 09:56:44 +0100

    M8: Added linting to the project

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './site.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('0,0.00');
-console.log(`Hello World: Current Value: ${courseValue}.`);
+document.getElementById("value").innerText = `Hello World: Current Value: ${courseValue}.`;
